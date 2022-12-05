b8a29130de50e7eb2eed5d6b53c7ff5798596913
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-12-04 22:14:47 -0300

    More modifications and additions

--- philo/main.c
+++ philo/main.c
@@ -6,34 +6,180 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:24:27 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/12/01 19:52:20 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/12/04 22:11:01 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "metaphysics.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 int	check_input_size(int argc);
-int	check_input(int argc, char **argv);
-int	check_input_values(int argc, char **argv);
+int	get_input(int argc, char **argv, int *values);
+int	check_input_values(int argc, char **argv, int *values);
 
 #ifndef U_INT_MAX
 # define U_INT_MAX 4294967295
 #endif 
 
-int	main(int argc, char **argv)
+// tv_sec seconds since epoch
+// usleep(microseconds (1,000,000 of a second)
+void	print_status(t_philo *philosopher, char *status)
 {
-	int	error;
-	
-	error = check_input(argc, argv);
-	if (error != 0)
-		return (error);
+	struct timeval	current_time;
+	unsigned long	timestamp;
+
+	pthread_mutex_lock(philosopher->print_mutex);
+	gettimeofday(&current_time, NULL);
+	timestamp = (current_time.tv_sec * 1000) - philosopher->matrix_start;
+	printf("[%ld] %d %s\n", timestamp, philosopher->philo_id, status);
+	pthread_mutex_unlock(philosopher->print_mutex);
+}
+
+void	*simulation(void *param)
+{
+	t_philo			*philosopher;
+	struct timeval	current_time;
+	long			lt_eat_msec;
+	long			present_time;
+
+	philosopher = (t_philo *) param;
+	gettimeofday(&current_time, NULL);
+	lt_eat_msec = current_time.tv_sec * 1000;
+ 	while (1)
+	{
+		gettimeofday(&current_time, NULL);
+		present_time = current_time.tv_sec;
+		if ((present_time - lt_eat_msec) < (philosopher->tt_die / 1000))
+		{
+			print_status(philosopher, "is thinking");
+		}
+		else
+			break ;
+	}
+	printf("Philosopher %d is dead\n", philosopher->philo_id);
+	return (NULL);
+}
+
+int	load_values(t_meta *philo_info, int *values)
+{
+	if (!philo_info || !values)
+		return (-1);
+	philo_info->n_of_philos = values[0];
+	philo_info->tt_die = values[1];
+	philo_info->tt_eat = values[2];
+	philo_info->tt_sleep = values[3];
+	philo_info->min_meals = values[4];
+	if (pthread_mutex_init(&philo_info->print_mutex, NULL) != 0)
+	{
+		put_err_str("{ERROR}: Could not start mutex print");
+		return (-1);
+	}
 	return (0);
 }
 
-int	check_input(int argc, char **argv)
+int	philo_create(t_philo **philos, t_meta *philo_info)
+{
+	unsigned int	i;
+
+	philos = malloc(sizeof(t_philo) * philo_info->n_of_philos);
+	if (!philos)
+		return (-1);
+	i = 0;
+	while (i < philo_info->n_of_philos)
+	{
+		philos[i]->philo_id = i;
+		philos[i]->tt_die = philo_info->tt_die;
+		philos[i]->tt_eat = philo_info->tt_eat;
+		philos[i]->tt_sleep = philo_info->tt_sleep;
+		philos[i]->min_meals = philo_info->min_meals;
+		philos[i]->state_change = 0;
+		philos[i]->state = PHILO_THINK; 
+		philos[i]->lt_eat = 0;
+		philos[i]->lt_sleep = 0;
+		philos[i]->print_mutex = &philo_info->print_mutex;
+		i++;
+	}
+	return (0);
+}
+
+int dinner_start(t_philo *philos, t_meta *philo_info, pthread_t *philo_threads)
+{
+	unsigned int		i;
+	struct timeval		start;
+	long				timestamp;
+
+	i = 0;
+	philo_threads = malloc(sizeof(pthread_t) * philo_info->n_of_philos);
+	if (!philo_threads)
+	{
+		put_err_str("{ERROR}: malloc malfunction\n");
+		return (-1);
+	}
+	gettimeofday(&start, NULL);
+	timestamp = start.tv_sec * 1000;
+	while (i < philo_info->n_of_philos)
+	{
+		philos[i].matrix_start = timestamp;
+		if (pthread_create(&philo_threads[i], \
+			 NULL, simulation, (void *) &philos[i]) != 0)
+		{
+			put_err_str("{ERROR}: error during thread creation\n");
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int dinner_end(t_philo *philos, pthread_t *philo_threads, int philo_size) 
+{
+	int	i;
+
+	i = 0;
+	while (i < philo_size)
+	{
+		pthread_join(philo_threads[i], NULL);
+		i++;
+	}
+	pthread_mutex_destroy(philos[0].print_mutex);
+	free(philos);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	static int	values[5] = {-1, -1, -1, -1, -1};	
+	int			op_code;
+	t_meta		philo_info;
+	t_philo		philos;
+	pthread_t	philo_threads;
+
+	op_code = get_input(argc, argv, values);
+	if (op_code != 0)
+		return (op_code);
+	op_code = load_values(&philo_info, values);
+	if (op_code != 0)
+		return (op_code);
+	op_code = philo_create(&philos, &philo_info);
+	if (op_code != 0)
+		return (op_code);
+	op_code = dinner_start(philos, &philo_info, philo_threads); 
+	if (op_code != 0)
+		return (op_code);
+	op_code = dinner_end(philos, philo_threads, philo_info.n_of_philos);
+	if (op_code != 0)
+		return (op_code);
+	return (0);
+}
+
+int	get_input(int argc, char **argv, int *values)
 {
 	char	*err_msg;
 	char	*usage;
+	int		ret_code;
 
 	usage = "{USAGE}: ./philo <nº of philosophers> <time to die> <time to eat> \
 		<time to sleep> [nº of times every philo must eat]\n";
@@ -44,22 +190,21 @@ int	check_input(int argc, char **argv)
 		put_err_str(usage);
 		return (1);
 	}
-	if (check_input_values(argc, argv) == 2)
+	ret_code = check_input_values(argc, argv, values);	
+	if (ret_code == 2)
 	{
 		err_msg = "{ERROR}: arg(s) must be greater than zero\n"; 
 		put_err_str(err_msg);
-		return (2);
 	}
-	if (check_input_values(argc, argv) == 3)
+	if (ret_code == 3)
 	{
 		err_msg = "{ERROR}: out of bounds integer value\n";
 		put_err_str(err_msg);
-		return (3);
 	}
-	return (0);
+	return (ret_code);
 }
 
-int	check_input_values(int argc, char **argv)
+int	check_input_values(int argc, char **argv, int *values)
 {
 	long long number; 
 	int		i;
@@ -70,8 +215,10 @@ int	check_input_values(int argc, char **argv)
 		number = ft_atoll(argv[i]);
 		if (number <= 0)
 			return (2);
-		if (number <= 0 || number > U_INT_MAX)
+		else if (number > U_INT_MAX)
 			return (3);
+		else
+			values[i - 1] = (unsigned int) number;
 		i++;
 	}
 	return (0);
--- philo/metaphysics.h
+++ philo/metaphysics.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:09:20 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/12/01 19:13:29 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/12/04 22:02:44 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,53 @@
 # include <pthread.h>
 # include <unistd.h>
 
+#ifndef PHILO_SLEEP
+# define PHILO_SLEEP 1
+#endif
+
+#ifndef PHILO_EAT
+# define PHILO_EAT 2
+#endif
+
+#ifndef PHILO_THINK
+# define PHILO_THINK 3
+#endif
+
+#ifndef PHILO_DEAD
+# define PHILO_DEAD 4
+#endif
+
 typedef struct s_platonic_philo
 {
-	int	tt_eat;
-	int	tt_die;
-	int	tt_think;
-	int	tt_sleep;
-	int	min_meals;
+	unsigned int	n_of_philos;
+	unsigned int	tt_die;
+	unsigned int	tt_eat;
+	unsigned int	tt_sleep;
+	unsigned int	min_meals;
+	unsigned long	matrix_start;
+	pthread_mutex_t	print_mutex;
 }	t_meta;
 
 typedef struct s_thinker
 {
-	int	philo_id;
-	int	tt_eat;
-	int	tt_die;
-	int	tt_think;
-	int	tt_sleep;
-	int	min_meals;
+	unsigned int	philo_id;
+	unsigned int	tt_die;
+	unsigned int	tt_eat;
+	unsigned int	tt_sleep;
+	unsigned int	min_meals;
+	unsigned int	lt_eat;
+	unsigned int	lt_sleep;
+	unsigned int	state_change:1;
+	unsigned int	state;
+	unsigned long	matrix_start;
+	pthread_mutex_t	*print_mutex;
 }	t_philo;
 
 typedef struct s_dinning_table
 {
 	pthread_mutex_t	*forks;
 	int				num_of_forks;
+	unsigned int	simulation_start;
 }	t_table;
 
 // utils
