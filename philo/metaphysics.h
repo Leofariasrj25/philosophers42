87de7905cb4198fd013ed8038c3022e878c5da98
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-12-09 18:30:19 -0300

    Update header to include nem functions

--- philo/metaphysics.h
+++ philo/metaphysics.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:09:20 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/12/05 15:40:53 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/12/09 18:22:40 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,33 +52,45 @@ typedef struct s_thinker
 	unsigned int	tt_sleep;
 	unsigned int	min_meals;
 	unsigned int	lt_eat;
-	unsigned int	lt_sleep;
+ 	unsigned int	lt_sleep;
 	unsigned int	state_change:1;
 	unsigned int	state;
+	unsigned int	has_lfork:1;
+	unsigned int	has_rfork:1;
 	unsigned long	matrix_start;
 	pthread_mutex_t	*print_mutex;
+	pthread_mutex_t **forks;
 }	t_philo;
 
-typedef struct s_dinning_table
+typedef struct s_perfect_fork
 {
 	pthread_mutex_t	*forks;
 	int				num_of_forks;
 	unsigned int	simulation_start;
-}	t_table;
+}	t_fork;
 
 // simulation
-pthread_t	**dinner_start(t_philo *philos, t_meta *philo_info, int *op_code);
-int			dinner_end(t_philo *philos, pthread_t **philo_threads, int philo_size);
-void		*simulation(void *param);
-t_philo		*philo_create(t_meta *philo_info);
+t_philo			*philo_create(t_meta *philo_info);
+pthread_t		**dinner_start(t_philo *philos, t_meta *philo_info, int *op_code);
+int				dinner_end(t_philo *philos, pthread_t **philo_threads, int philo_size);
+void			*simulation(void *param);
 
+// philo actions
+int				philo_sleep(t_philo *philosopher);
+int				philo_eat_meal(t_philo *philosopher);
+int				philo_take_lfork(t_philo *philosopher);
+int				philo_take_rfork(t_philo *philosopher);
+	
+// time
+unsigned int	elapsed_time_ms(unsigned int timestamp_ms);
+int				tstamp_cmp_ms(unsigned int ts1, unsigned int ts2);
 //input
-int			get_input(int argc, char **argv, int *values);
-int			load_values(t_meta *philo_info, int *values);
+int				get_input(int argc, char **argv, int *values);
+int				load_values(t_meta *philo_info, int *values);
 
 // output 
-void		print_status(t_philo *philosopher, char *msg);
-void		put_str_fd(int fd, char *c);
-void		put_err_str(char *c);
-long long	ft_atoll(char *str);
+void			print_status(t_philo *philosopher, char *msg);
+void			put_str_fd(int fd, char *c);
+void			put_err_str(char *c);
+long long		ft_atoll(char *str);
 #endif
